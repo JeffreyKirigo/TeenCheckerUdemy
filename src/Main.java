0c18ab771b86e00b019012eb99197478de60de8a
JeffreyKirigo <jeffreykirigo75@gmail.com>
2019-11-02 13:09:03 +0300

    simplyfied

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(TeenNumberChecker.hasTeen(23,16,24));
+        System.out.println(TeenNumberChecker.hasTeen(23,23,24));
     }
 }
--- src/TeenNumberChecker.java
+++ src/TeenNumberChecker.java
@@ -2,7 +2,7 @@ public class TeenNumberChecker {
 
         public static boolean hasTeen(int a,int b,int c){
 
-            return (((a>=13 || a<=19)||(b>=13 || b<=19)||(c>=13 || c<=19))? true:false);
+            return ((a >= 13 && a <= 19) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19));
         }
     }
 
